6e10a9c52676cc6e2412401866737592a02f6a8a
Rinalon <mit.krist@yandex.ru>
2026-08-22 00:30:41 +0300

    helpers: remake email generation logic Emails generation according to the template 'test_<timestamp>_<host>@<domain>' timestamp from datetime.now <host>@<domain> from faker.email()

--- helpers/user_data.py
+++ helpers/user_data.py
@@ -1,12 +1,15 @@
 from faker import Faker
 from pygments.lexers import data
+from datetime import datetime
 
 
 def generate_user(faker: Faker, exclude: tuple[str] | None = None) -> dict:
+    now = datetime.now().strftime("%Y%m%d_%H%M%S")
+
     data = {
         "first_name": faker.first_name(),
         "last_name": faker.last_name(),
-        "email": faker.email(),
+        "email": f"test_{now}_{faker.email()}",
         "phone": faker.phone_number(),
         "password": faker.password(length=10, special_chars=True, digits=True),
     }
