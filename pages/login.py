d621744062e1ee2c55375b639a9875e967299183
Rinalon <mit.krist@yandex.ru>
2026-08-22 11:38:49 +0300

    pages: make LoginPage inherit from BasePage - add BasePage as parent LoginPage - add active methods: -- fill_email -- fill_password -- click_login -- login

--- pages/login.py
+++ pages/login.py
@@ -1,7 +1,27 @@
+from playwright.sync_api import expect
+from pages.base import BasePage
 
-class LoginPage():
+class LoginPage(BasePage):
     def __init__(self, page):
-        self.page = page
+        super().__init__(page)
+
         self.email_input = page.locator("input[type=\"email\"]")
         self.password_input = page.locator("input[type=\"password\"]")
         self.login_button = page.get_by_role("button", name="Войти")
+
+    def fill_email(self, email):
+        self.email_input.fill(email)
+        return self
+
+    def fill_password(self, password):
+        self.password_input.fill(password)
+        return self
+
+    def click_login(self):
+        self.login_button.click()
+        return self
+
+    def login(self, email, password):
+        self.fill_email(email).fill_password(password).click_login()
+        expect(self.page).to_have_url("https://archiscope.ru/", timeout=10000)
+        return self.page
