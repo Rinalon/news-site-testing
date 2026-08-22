aafecb1db551da7c5557f648c9761c237bba148b
Rinalon <mit.krist@yandex.ru>
2026-08-22 13:37:50 +0300

    pages:  make NewsCreatingPage inherit from BasePage - add BasePage as parent NewsCreatingPage - add fill_form method

--- pages/news_creating.py
+++ pages/news_creating.py
@@ -1,7 +1,9 @@
+from pages.base import BasePage
 
-class NewsCreatingPage:
+class NewsCreatingPage(BasePage):
     def __init__(self, page):
-        self.page = page
+        super().__init__(page)
+
         self.title_input = page.locator("input[name=\"title\"]")
         self.subtitle_input = page.locator("input[name=\"subtitle\"]")
         self.text_input = page.locator("textarea[name=\"text\"]")
@@ -9,3 +11,22 @@ class NewsCreatingPage:
         self.img_load = page.locator("input[type=\"file\"]")
         
         self.submit_button = page.locator("button[type=\"submit\"]")
+
+    def fill_form(
+            self,
+            title: str | None = "",
+            subtitle: str | None = "",
+            text: str | None = "",
+            tags: str | None = "",
+            image: str | None = None,
+    ):
+        self.title_input.fill(title)
+        self.subtitle_input.fill(subtitle)
+        self.text_input.fill(text)
+        self.tags_input.fill(tags)
+
+        if image:
+            self.img_load.set_input_files(image)
+
+        self.submit_button.click()
+        return self.page
